39271205def02f3aefdf81562a7e4216ecfdc14d
Jacob Hanna <jacob@Jacobs-MacBook-Pro.local>
2019-10-08 19:19:32 -0700

    updated

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO subs (sub_id, sub_name, sub_price) VALUES (1, "Spotify Premium", "$10"), (2, "Spotify Family Premium", "$15"), (3, "YouTube", "$12"), (4, "HBO GO", "$15"), (5, "Hulu Basic", "$6"), (6, "Hulu Premium", "$12"), (7, "Hulu Basic + Live TV", "$45"), (8, "Hulu Premium + Live", "$51"), (9, "Netflix Single Plan", "$9"), (10, "Netflix Family Plan", "16");
+
+INSERT INTO users (user_id, full_name, user_name, email_e, user_subs) VALUES (1, "Jacob Hanna", "jacobh", "jacob@gmail.com", "1"), (2, "Justin Goyena", "justingo", "justin@gmail.com", "8"), (3, "Nicholai Casey", "ncasey", "nicholai@gmail.com", "8");
